b637ae600289031137385d7c4d5eb9183cfb8358
a123aos <269692899+a123aos@users.noreply.github.com>
2026-08-14 13:06:24 +0800

    feat: include instant queue data and views

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,16 @@
 {
     "name": "Odoo Email Rate Limit",
     "version": "19.0.1.0.0",
-    "summary": "Per outgoing mail server rate limiting with SMTP fallback",
+    "summary": "Per outgoing mail server rate limiting with instant queue and SMTP fallback",
     "category": "Technical",
     "license": "LGPL-3",
     "author": "Aritrz",
     "depends": ["mail"],
     "data": [
-        "views/ir_mail_server_views.xml"
+        "security/ir.model.access.csv",
+        "views/ir_mail_server_views.xml",
+        "views/email_rate_queue_views.xml",
+        "data/ir_cron.xml",
     ],
     "installable": true,
     "application": false
