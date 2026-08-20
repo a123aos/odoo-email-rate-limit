56da8315f4365b234b1ece77774173a1012baf69
a123aos <269692899+a123aos@users.noreply.github.com>
2026-08-20 13:37:47 +0800

    Register OWL dashboard assets and action

--- odoo_email_rate_limit/__manifest__.py
+++ odoo_email_rate_limit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Email Rate Limit",
-    "version": "19.0.2.1.1",
+    "version": "19.0.2.1.2",
     "category": "Discuss/Email",
     "summary": "Lark-compatible email rate limits, sender pools and delayed queue",
     "depends": ["mail"],
@@ -8,8 +8,15 @@
         "security/ir.model.access.csv",
         "views/ir_mail_server_views.xml",
         "views/email_queue_views.xml",
+        "views/rate_limit_dashboard_action.xml",
         "data/ir_cron.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "odoo_email_rate_limit/static/src/js/rate_limit_dashboard.js",
+            "odoo_email_rate_limit/static/src/xml/rate_limit_dashboard.xml",
+        ],
+    },
     "installable": True,
     "application": False,
     "license": "LGPL-3",
