149833d34fc410761e3f10d8d15d347d68dd0ab8
a123aos <269692899+a123aos@users.noreply.github.com>
2026-08-22 22:15:43 +0800

    Assign new signups to a daily signup mail server pool

--- models/res_users.py
+++ models/res_users.py
@@ -8,14 +8,13 @@ class ResUsers(models.Model):
 
     @api.model
     def _signup_create_user(self, values):
-        # An uninvited signup creates a new customer. Assign the signup pool
-        # before the signup confirmation email is generated/sent.
         is_new_external_signup = not values.get('partner_id')
         user = super()._signup_create_user(values)
         partner = user.partner_id
-        if is_new_external_signup and partner and not partner.email_sender_pool:
+        if is_new_external_signup and partner:
+            server = self.env['mail.mail']._allocate_sender_pool('signup')
             partner.sudo().write({
-                'email_sender_pool': self.env['mail.mail']._allocate_sender_pool('signup'),
+                'email_sender_server_id': server.id,
                 'email_sender_pool_date': datetime.now(timezone.utc).date(),
             })
         return user
