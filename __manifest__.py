1384475763a68b45658ef8bc398df7751b221380
a123aos <269692899+a123aos@users.noreply.github.com>
2026-08-22 22:56:52 +0800

    [sender-pool] Register template sender pool view and bump version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Email Rate Limit & Customer Sender Pools',
-    'version': '19.0.4.0.0',
+    'version': '19.0.5.0.0',
     'category': 'Technical/Email',
     'summary': 'Daily customer sender pools for outgoing email',
     'depends': ['mail', 'auth_signup'],
@@ -8,6 +8,7 @@
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
         'views/ir_mail_server_views.xml',
+        'views/mail_template_views.xml',
     ],
     'installable': True,
     'application': False,
